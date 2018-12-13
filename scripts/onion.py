4b31e01540a74907eecfc62dc90ce87f2fcaa1aa
Pablo Alejandro <pabloabur@gmail.com>
2018-12-12 22:39:28 -0200

    preliminary onion skin text

--- scripts/onion.py
+++ scripts/onion.py
@@ -2,13 +2,26 @@ import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
 import os
+from scipy.interpolate import UnivariateSpline
 
-# File settings
+###### File settings
 figsFolder = '/home/pablo/git/master-thesis/figuras/'
-path = '/home/pablo/osf/Master-Thesis-Data/population/onion/false_decay/trial1'
+trial = 2
+path = '/home/pablo/osf/Master-Thesis-Data/population/onion/false_decay/trial'+ str(trial)
+if trial==1:
+    filenameonionwithoutRC = 'onionwithoutRC'
+    filenameonionwithRC = 'onionwithRC'
+    filenamesonion = [filenameonionwithoutRC, filenameonionwithRC]
+    filenameoniondvdt = 'oniondvdt'
+elif trial==2:
+    filenameonionwithoutRC = 'onionwithoutRC2'
+    filenameonionwithRC = 'onionwithRC2'
+    filenamesonion = [filenameonionwithoutRC, filenameonionwithRC]
+    filenameoniondvdt = 'oniondvdt2'
 os.chdir(path)
 
-inputFreqs = [90, 100, 110, 130, 140, 150, 170, 180, 190, 210]
+###### Simulation settings and variables
+inputFreqs = [150, 300, 450, 600, 750, 900, 1050, 1200, 1350, 1500]
 qntdInEachGroup = 3
 availableS = [x for x in range(1, 76)]
 availableFR = [x for x in range(76, 151)]
@@ -17,65 +30,80 @@ availableFF = [x for x in range(151, 301)]
 SIndex = np.random.choice(availableS, qntdInEachGroup, replace=False)
 FRIndex = np.random.choice(availableFR, qntdInEachGroup, replace=False)
 FFIndex = np.random.choice(availableFF, qntdInEachGroup, replace=False)
-Indexes = np.concatenate((SIndex, FRIndex, FFIndex), axis=None)
-Indexes.sort(axis=0)
+indexes = np.concatenate((SIndex, FRIndex, FFIndex), axis=None)
+indexes.sort(axis=0)
+dvdtMN = np.random.choice(indexes)
+print(indexes)
+print(dvdtMN)
 
-# Without RC
-# Preparing plot
-for i, inputFreq in enumerate(inputFreqs):
-    unitNumber = []
-    spikeInstant = []
-    filename = 'output'+str(i)+'.dat'
-    f = open(filename, 'r')
-    lines = f.readlines()
-    for line in lines:
-        spikeInstant.append(float(line.split()[0]))
-        unitNumber.append(int(float(line.split()[1])))
-
-plt.figure()
-plt.title('Sem célula de Renshaw')
-plt.xlabel('Tempo (ms)')
-plt.ylabel('Taxa de disparo instantâneo (pps)')
+meanFR = np.zeros(len(inputFreqs))
+dyo = np.zeros(len(inputFreqs), np.float)
+dyc = np.zeros(len(inputFreqs), np.float)
 
 # Normalize data to be used by color map
-norm = matplotlib.colors.Normalize(vmin=np.min(Indexes), vmax=np.max(Indexes))
+norm = matplotlib.colors.Normalize(vmin=np.min(indexes), vmax=np.max(indexes))
 
 # Choose color map
-colorMap = matplotlib.cm.Greys
+colorMap = matplotlib.cm.tab20
 
 # create a ScalarMappable and initialize a data structure
 mappable = matplotlib.cm.ScalarMappable(cmap=colorMap, norm=norm)
 mappable.set_array([])
 
-for index in Indexes:
-    ppsO = []
-    MNO = []
-    MNO = [y for x, y in enumerate(spikeInstantMNo) if unitNumberMNo[x]==index]
+simTypes = ['o', 'c'] # Without and with RC
 
-    for j in range(len(MNO)-1):
-        ppsO = np.append(ppsO, [1000/(MNO[j+1]-MNO[j])])
-    del MNO[0]
+for filenameonion, simType in zip(filenamesonion, simTypes):
+    # Preparing plot
+    plt.figure()
+    plt.xlabel('Taxa de disparo do comando descendente (pps)')
+    plt.ylabel('Taxa de disparo média do motoneurônio (pps)')
 
-    plt.step(MNO, ppsO, color=mappable.to_rgba(index))
-plt.colorbar(mappable)
-plt.show()
+    ###### Simulation
+    for index in indexes:
+        for i, inputFreq in enumerate(inputFreqs):
+            FR = np.array([])
+            unitNumber = []
+            spikeInstant = []
+            filename = 'output' + str(i) + simType + '.dat'
+            f = open(filename, 'r')
+            lines = f.readlines()
+            for line in lines:
+                spikeInstant.append(float(line.split()[0]))
+                unitNumber.append(int(float(line.split()[1])))
 
-## With RC
-## Preparing plot
-#plt.figure()
-#plt.title('Com célula de Renshaw')
-#plt.xlabel('Tempo (ms)')
-#plt.ylabel('Taxa de disparo instantâneo (pps)')
-#
-#for index in Indexes:
-#    ppsC = []
-#    MNC = []
-#    MNC = [y for x, y in enumerate(spikeInstantMNc) if unitNumberMNc[x]==index]
-#
-#    for j in range(len(MNC)-1):
-#        ppsC = np.append(ppsC, [1000/(MNC[j+1]-MNC[j])])
-#    del MNC[0]
-#
-#    plt.step(MNC, ppsC, color=mappable.to_rgba(index))
-#plt.colorbar(mappable)
+            MN = [y for x, y in enumerate(spikeInstant) if unitNumber[x]==index]
+            steadyMN = [x for x in MN if x>1000]
+
+            # Calculate instantaneous firing rate
+            for k in range(len(steadyMN)-1):
+                FR = np.append(FR, [1000 / (steadyMN[k+1] - steadyMN[k])])
+            # Calculate the mean
+            if not FR.size:
+                meanFR[i] = 0
+            else:
+                meanFR[i] = np.mean(FR)
+
+            # Getting values for next plot
+            if index==dvdtMN:
+                if simType=='o':
+                    dyo[0:-1] = np.diff(meanFR)/np.diff(inputFreqs)
+                    dyo[-1] = (meanFR[-1] - meanFR[-2])/(inputFreqs[-1] - inputFreqs[-2])
+                if simType=='c':
+                    dyc[0:-1] = np.diff(meanFR)/np.diff(inputFreqs)
+                    dyc[-1] = (meanFR[-1] - meanFR[-2])/(inputFreqs[-1] - inputFreqs[-2])
+    
+        #import pdb; pdb.set_trace()
+        plt.plot(inputFreqs, meanFR, linewidth=2, color=mappable.to_rgba(index))
+    plt.colorbar(mappable)
+    plt.savefig(figsFolder + filenameonion + '.svg', format='svg')
+
+plt.figure()
+plt.plot(inputFreqs, dyo,
+         label='Motoneurônio {} sem célula de Renshaw'.format(dvdtMN))
+plt.plot(inputFreqs, dyc,
+         label='Motoneurônio {} com célula de Renshaw'.format(dvdtMN))
+plt.legend()
+plt.xlabel('Taxa de disparo das fibras descendentes (pps)')
+plt.ylabel('Derivada da taxa de disparo')
 #plt.show()
+plt.savefig(figsFolder + filenameoniondvdt + '.svg', format='svg')
