53e014d0a5a2d37e894501496272bfb10e3730a3
pabloabur <pabloabur@gmail.com>
2019-06-21 19:38:27 -0300

    stopped at circuit description

--- presentation/pres.py
+++ presentation/pres.py
@@ -3,9 +3,8 @@ sys.path.append('/home/pablo/beampy-master')
 
 from beampy import *
 
-# We first create a new document for our presentation
-# Remove quiet=True to see Beampy compiler output
-doc = document(theme="mytheme", quiet=True)
+#doc = document(theme="mytheme", quiet=True)
+doc = document(quiet=True)
 
 with slide():
     maketitle(r'\textsc{Modelagem de células de Renshaw e sua utilização em simulações do sistema neuromuscular}',
@@ -21,7 +20,7 @@ with slide('A circuitaria da medula espinhal'):
         figure('figures/coluna.png', width=250)
     with group(x='auto', y='center') as g2:
         figure('figures/slice.png', width=200)
-    text('Fonte: Bear, Connors e Paradiso (2016)', y=500)
+    text('Fonte: Bear, Connors e Paradiso (2016)', y=500, size=15)
 
 with slide('A circuitaria da medula espinhal'):
     with group(x='auto', y='center') as g1:
@@ -31,7 +30,10 @@ with slide('A circuitaria da medula espinhal'):
                  'Tipos de unidades motoras',
                  r'Tamanho $\times$ tipo'],
                  width=300)
-    text('Fonte: Bear, Connors e Paradiso (2016)', x=60, y=450, width=250)
+    text('Fonte: Bear, Connors e Paradiso (2016)', x=80, y=450, width=250, size=15)
+
+with slide('A circuitaria da medula espinhal'):
+    figure('figures/circuit.png', width=450)
 
 section('Metodologia')
 section('Resultados e discussões')
